0677f97fe06ebb56aeedbb0d9e5d73acc436ddb4
Casey <innerforest7@gmail.com>
2020-10-23 16:42:52 -0700

    add MainPage

---
+++ react-ui/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <div id={"main-page"}>
+        <Audio className={"auido-element"}></Audio>
+      </div>
+    </>
+  )
+}
+export default MainPage;
