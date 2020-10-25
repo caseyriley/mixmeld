947d37babf81161e448a7947b9d16234dff66765
Casey <innerforest7@gmail.com>
2020-10-24 21:51:47 -0700

    delete App.css

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import MainPage from './components/MainPage';
 
 function App() {
