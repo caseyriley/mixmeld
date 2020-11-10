ecfa17513a247dceb5a1b43b26b5e21d9c73771f
Casey <innerforest7@gmail.com>
2020-11-09 20:34:55 -0800

    add play head functionality

--- react-ui/src/components/MainPage.js
+++ react-ui/src/components/MainPage.js
@@ -14,14 +14,13 @@ const stop = document.querySelector('.stop');
 
 const timerWrapper = document.querySelector('.timer');
 const timer = document.querySelector('.timer span');
-const timerBar = document.querySelector('.timer div');
+const timerBar = document.querySelector('.audio__bottom__playhead__left');
 
-const [timeState, setTimeState] = useState(1);
-const playTime = useRef(0);
-// let currentTime = playTime.current;
+const [timeState, setTimeState] = useState(":");
+
+
 
 useEffect(()=>{
-  // playTime.current = playTime.current + 1;
   setInterval(() => {
     console.log("wooop")
     
@@ -49,52 +48,16 @@ useEffect(()=>{
       // currentTime = mediaTime;
       // currentTime += 1;
       // timer.textContent = mediaTime;
-      // let barLength = timerWrapper.clientWidth * (media.currentTime/media.duration);
-      // timerBar.style.width = barLength + 'px';
+      let barLength = timerWrapper.clientWidth * (media.currentTime/media.duration);
+      timerBar.style.width = barLength + 'px';
     }
   }, 500);
 }, [timeState])
 
-// function updateTime(){
-//   setInterval(() => {
-//     console.log("wooop")
-//     playTime.current = playTime.current + 1;
-//     if (media){
-//       let minutes = Math.floor(media.currentTime / 60);
-//       let seconds = Math.floor(media.currentTime - minutes * 60);
-   
-//       let minuteValue;
-//       let secondValue;
-    
-//       if (minutes < 10) {
-//         minuteValue = '0' + minutes;
-//       } else {
-//         minuteValue = minutes;
-//       }
-    
-//       if (seconds < 10) {
-//         secondValue = '0' + seconds;
-//       } else {
-//         secondValue = seconds;
-//       }
-    
-//       let mediaTime = minuteValue + ':' + secondValue;
-//       // setTimeState(mediaTime)
-//       // currentTime = mediaTime;
-//       currentTime += 1;
-    
-//       // timer.textContent = mediaTime;
-    
-//       // let barLength = timerWrapper.clientWidth * (media.currentTime/media.duration);
-//       // timerBar.style.width = barLength + 'px';
-  
-//     }
-    
-//   }, 500);
-// }
+
 // ---------------Play/Pause-Button---------------
 function playPauseMedia() {
-  setTimeState(0)
+  setTimeState("00:00")
   // updateTime();
   // rwd.classList.remove('active');
   // fwd.classList.remove('active');
@@ -124,49 +87,7 @@ function stopMedia() {
 }
 // -----------------------------------------------
 
-// media.addEventListener('timeupdate', setTime);
 
-// useEffect (() =>{
-//   function setTime() {
-//     if (media){
-//       let minutes = Math.floor(media.currentTime / 60);
-//       let seconds = Math.floor(media.currentTime - minutes * 60);
-   
-//       let minuteValue;
-//       let secondValue;
-    
-//       if (minutes < 10) {
-//         minuteValue = '0' + minutes;
-//       } else {
-//         minuteValue = minutes;
-//       }
-    
-//       if (seconds < 10) {
-//         secondValue = '0' + seconds;
-//       } else {
-//         secondValue = seconds;
-//       }
-    
-//       let mediaTime = minuteValue + ':' + secondValue;
-//       // setTimeState(mediaTime)
-//       playTime.current = mediaTime
-//       // timer.textContent = mediaTime;
-    
-//       // let barLength = timerWrapper.clientWidth * (media.currentTime/media.duration);
-//       // timerBar.style.width = barLength + 'px';
-  
-//     }
-//   }
-//   setTime();
-// }, timeState)
-
-
- 
-
-  // function play(){
-  //   const audio = document.getElementById("audio");
-  //   audio.audioTracks[0].enabled = true;
-  // }
 
   return (
     <>
@@ -206,7 +127,7 @@ function stopMedia() {
             </div>
             {/* <span id={"audio__bottom__time__end"} >{timeState}</span> */}
             <div id={"audio__bottom__playhead"} >
-              <div id={"audio__bottom__playhead__left"} ></div>
+              <div  className={"audio__bottom__playhead__left"} ></div>
             </div>
 
           </div>
