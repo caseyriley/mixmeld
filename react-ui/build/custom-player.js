1157bb648706c84731cb3461c4a8a99572a4ca02
Casey <innerforest7@gmail.com>
2021-01-07 21:07:51 -0800

    ungitignore build

---
+++ react-ui/build/custom-player.js
@@ -0,0 +1,28 @@
+
+
+// window.addEventListener('DOMContentLoaded', event => {
+//   const media = document.querySelector('audio');
+//   const controls = document.querySelector('.controls');
+//   const play = document.querySelector('.play');
+//   const stop = document.querySelector('.stop');
+//   media.removeAttribute('controls');
+//   controls.style.visibility = 'visible';
+
+//   play.addEventListener('click', playPauseMedia);
+
+
+//   function playPauseMedia() {
+//     rwd.classList.remove('active');
+//     fwd.classList.remove('active');
+//     clearInterval(intervalRwd);
+//     clearInterval(intervalFwd);
+//     if(media.paused) {
+//       play.setAttribute('data-icon','u');
+//       play.src = '../images/pauseButton.png';
+//       media.play();
+//     } else {
+//       play.setAttribute('data-icon','P');
+//       media.pause();
+//     }
+//   }
+// })
