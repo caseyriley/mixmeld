52633b09ba54801c6773dfc59014e9aa43499cb6
Casey <innerforest7@gmail.com>
2021-01-11 13:28:28 -0800

    update console.log

--- react-ui/src/config.js
+++ react-ui/src/config.js
@@ -9,6 +9,6 @@
 // export let API_URL = process.env.FLASK_ENV === 'development' ? 'http://localhost:5000' : 'https://mixmeld.herokuapp.com';
 export let API_URL = process.env.REACT_APP_BASE_URL
 
-console.log(API_URL)
+console.log("API_URL", API_URL)
 
 console.log('new-build')
