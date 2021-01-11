e53e7bd8daf5c7d3f4daf3dc7913ab1dcd44576d
Casey <innerforest7@gmail.com>
2021-01-11 00:12:52 -0800

    add API_URL ternary to config.js

--- react-ui/src/config.js
+++ react-ui/src/config.js
@@ -1,5 +1,8 @@
 
-export const API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000'
+// export const API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000'
 // export const API_URL = 'http://localhost:5000'
-
+export let API_URL = 'http://localhost:5000';
+if (process.env.FLASK_ENV != 'development') {
+  let API_URL = 'https://mixmeld.herokuapp.com';
+} ;
 
