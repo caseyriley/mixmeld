3336ebea5c3db4c38530cd529db6764fac42ad4c
Casey <innerforest7@gmail.com>
2021-01-11 00:27:44 -0800

    add ternary to config.js

--- react-ui/src/config.js
+++ react-ui/src/config.js
@@ -1,8 +1,9 @@
 
 // export const API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000'
 // export const API_URL = 'http://localhost:5000'
-export let API_URL = 'http://localhost:5000';
-if (process.env.FLASK_ENV !== 'development') {
-  let API_URL = 'https://mixmeld.herokuapp.com';
-};
+// export let API_URL = 'http://localhost:5000';
+// if (process.env.FLASK_ENV !== 'development') {
+//   let API_URL = 'https://mixmeld.herokuapp.com';
+// };
 
+export let API_URL = process.env.FLASK_ENV === 'development' ? 'http://localhost:5000' : 'https://mixmeld.herokuapp.com';
