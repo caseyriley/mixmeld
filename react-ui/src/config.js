190cf5c55be8656d1d3f0c1ecb00795ae71878c1
Casey <innerforest7@gmail.com>
2021-01-11 00:20:46 -0800

    edit config.js

--- react-ui/src/config.js
+++ react-ui/src/config.js
@@ -2,7 +2,7 @@
 // export const API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000'
 // export const API_URL = 'http://localhost:5000'
 export let API_URL = 'http://localhost:5000';
-if (process.env.FLASK_ENV != 'development') {
+if (process.env.FLASK_ENV !== 'development') {
   let API_URL = 'https://mixmeld.herokuapp.com';
-} ;
+};
 
