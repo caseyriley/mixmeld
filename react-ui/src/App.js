a281127e5f06e1f08ad9abe9272a4fabea461084
Casey <innerforest7@gmail.com>
2021-01-13 17:43:48 -0800

    add new .env path in App.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -4,7 +4,7 @@ import Login from './components/Login';
 import MainPage from './components/MainPage';
 
 function App() {
-  require('dotenv').config();
+  require('dotenv').config({ path: "../../.env" });
   console.log('REACT_APP_BASE_URL1', process.env.REACT_APP_BASE_URL)
   const loggedIn = window.localStorage.getItem("auth_token");
 
