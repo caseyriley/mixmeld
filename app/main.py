6554cc24e5bb7e515d3dd93eccccc7205d3a67a8
Casey <innerforest7@gmail.com>
2021-01-17 21:02:39 -0800

    change favicon route

--- app/main.py
+++ app/main.py
@@ -60,4 +60,4 @@ def api():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-        'favicon.ico',mimetype='image/vnd.microsoft.icon')
+        'favicon.ico')
