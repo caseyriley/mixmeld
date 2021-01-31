0b71ed77268a8617c39e6e6cef7eda1e8ee8db05
Casey <innerforest7@gmail.com>
2021-01-30 22:23:33 -0800

    add browserSupport.js

---
+++ react-ui/public/browserSupport.js
@@ -0,0 +1,8 @@
+// window.addEventListener("DOMContentLoaded", ()=>{
+//   const rangeElement = document.getElementById('range-input');
+//   rangeElement.addEventListener('touchmove', function(event){
+//     event.preventDefault();
+    
+//   });   
+//   console.log("browser support", rangeElement)
+// })
