b3d47e034fdddce108499d2b4a18d7364e80b4ff
Casey <innerforest7@gmail.com>
2021-01-30 22:36:07 -0800

    mody volume range input

--- react-ui/public/browserSupport.js
+++ react-ui/public/browserSupport.js
@@ -1,8 +1,10 @@
-// window.addEventListener("DOMContentLoaded", ()=>{
-//   const rangeElement = document.getElementById('range-input');
-//   rangeElement.addEventListener('touchmove', function(event){
-//     event.preventDefault();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // console.log('DOM fully loaded and parsed');
+  const rangeElement = document.getElementById('range-input');
+  rangeElement.addEventListener('touchmove', function(event){
+  event.preventDefault();
     
-//   });   
-//   console.log("browser support", rangeElement)
-// })
+  });   
+  console.log("browser support", rangeElement)
+});
--- react-ui/src/components/VolumeUiSlider.js
+++ react-ui/src/components/VolumeUiSlider.js
@@ -9,8 +9,8 @@ const VolumeUiSlider = (props)=> {
           <img src={texture} alt=""/>
         </span>
       </span>
-      <div className={"pl2-volume-level"} ref={props.volumeLevel}></div>
-      <input className={"pl2-slider-input"} type={"range"} min={"0"} max={"100"} step={"1"} ref={props.volumeSlider} onChange={props.changeVolume}></input>
+    <div className={"pl2-volume-level"} ref={props.volumeLevel}></div>
+      <input id={"range-input"} className={"pl2-slider-input"} type={"range"} min={"0"} max={"100"} step={"1"} ref={props.volumeSlider} onChange={props.changeVolume}></input>
     </div>
   )
 }
