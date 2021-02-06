8ee811926653b9654dabe27a144964793ecce753
Casey <innerforest7@gmail.com>
2021-02-05 21:27:38 -0800

    reposition and media screen audio controls

--- react-ui/src/components/TrackDisplay.js
+++ react-ui/src/components/TrackDisplay.js
@@ -39,32 +39,36 @@ const TrackDisplay = (props) => {
 
   return (
     <>
-      <div id={"pl2-track-display"}>
-        {/* <div>{media.current ? `${Math.floor(media.current.currentTime / 60)}:${Math.floor(media.current.currentTime - Math.floor(media.current.currentTime / 60) * 60)}` : ""}</div> */}
-                  
-        {props.trackEditState ? 
-        <> 
-          <UploadingNewImage currentUser={props.currentUser} refreshTrackState={props.refreshTrackState} 
-          setRefreshTrackState={props.setRefreshTrackState} setTrackArtState={props.setTrackArtState} 
-          setPl2TrackLocationState={props.setPl2TrackLocationState} pl2TrackLocationState={props.pl2TrackLocationState}
-          trackEditState={props.trackEditState} setTrackEditState={props.setTrackEditState}/>
-        </>
-        : ""}
-        <div className={"track-art-c"} >
-          <img className={`track-art ${largeImageState}`} src={`${props.trackArtState ? props.trackArtState : formlessMusicIcon}`} alt={""} onClick={toglleImageSize}/>
-        </div>
-        <div id={"pl2-audio__bottom"} >
-          <p id={"pl2-audio__top__song-name"}>{props.songNameState ? props.songNameState : `${props.firstTrack ? props.firstTrack[0].trackname : ""}`}</p>
-          {/* <p id={"pl2-audio__top__song-artist"}>{artistNameState ? artistNameState : `${firstTrack ? firstTrack[0].trackartist : ""}`}</p> */}
-          <p id={"pl2-audio__top__song-artist"}>{props.artistNameState ? props.artistNameState : ``}</p>
-        
-          <div className={"pl2-audio__bottom__playhead"} ref={audioBottomPlayhead} >
-            <input id={"pl2-playhead-input"} ref={props.playHeadSlider} type={"range"} min={"0"} max={"1"} step={"0.01"} onChange={props.movePlayheadOnClick} ></input>
-            <div className={"pl2-audio__bottom__playhead__left"} ref={timerBar} ></div>
-            <div className="pl2-timer">
-              <span id={"pl2-audio__bottom__time__start"} >{timeState}</span>
+      <div>
+
+      
+        <div id={"pl2-track-display"}>
+          {/* <div>{media.current ? `${Math.floor(media.current.currentTime / 60)}:${Math.floor(media.current.currentTime - Math.floor(media.current.currentTime / 60) * 60)}` : ""}</div> */}
+                    
+          {props.trackEditState ? 
+          <> 
+            <UploadingNewImage currentUser={props.currentUser} refreshTrackState={props.refreshTrackState} 
+            setRefreshTrackState={props.setRefreshTrackState} setTrackArtState={props.setTrackArtState} 
+            setPl2TrackLocationState={props.setPl2TrackLocationState} pl2TrackLocationState={props.pl2TrackLocationState}
+            trackEditState={props.trackEditState} setTrackEditState={props.setTrackEditState}/>
+          </>
+          : ""}
+          <div className={"track-art-c"} >
+            <img className={`track-art ${largeImageState}`} src={`${props.trackArtState ? props.trackArtState : formlessMusicIcon}`} alt={""} onClick={toglleImageSize}/>
+          </div>
+          <div id={"pl2-audio__bottom"} >
+            <p id={"pl2-audio__top__song-name"}>{props.songNameState ? props.songNameState : `${props.firstTrack ? props.firstTrack[0].trackname : ""}`}</p>
+            {/* <p id={"pl2-audio__top__song-artist"}>{artistNameState ? artistNameState : `${firstTrack ? firstTrack[0].trackartist : ""}`}</p> */}
+            <p id={"pl2-audio__top__song-artist"}>{props.artistNameState ? props.artistNameState : ``}</p>
+          
+            <div className={"pl2-audio__bottom__playhead"} ref={audioBottomPlayhead} >
+              <input id={"pl2-playhead-input"} ref={props.playHeadSlider} type={"range"} min={"0"} max={"1"} step={"0.01"} onChange={props.movePlayheadOnClick} ></input>
+              <div className={"pl2-audio__bottom__playhead__left"} ref={timerBar} ></div>
+              <div className="pl2-timer">
+                <span id={"pl2-audio__bottom__time__start"} >{timeState}</span>
+              </div>
+              <TimeRemaining media={props.media}/>
             </div>
-            <TimeRemaining media={props.media}/>
           </div>
         </div>
       </div>
