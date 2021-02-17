d9ad3667feef39e451fad6e694f8858b08032f14
Casey <innerforest7@gmail.com>
2021-02-16 18:15:44 -0800

    add fade-in animation

--- react-ui/src/setupProxy.js
+++ react-ui/src/setupProxy.js
@@ -15,8 +15,7 @@ module.exports = function (app) {
   app.use(
     "/auth/login",
     createProxyMiddleware({
-      target:
-        "5000",
+      target: "5000",
       headers: {
         accept: "application/json",
         methods: "POST"
